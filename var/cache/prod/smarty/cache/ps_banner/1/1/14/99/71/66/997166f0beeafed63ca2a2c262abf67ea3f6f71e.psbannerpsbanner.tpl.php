ec734c3190251755046687ed5490072185bf7637
ssnow <ss.nowacki@gmail.com>
2018-10-10 20:01:54 +0200

    Dodałem baner i slidery z odnosnikami

--- var/cache/prod/smarty/cache/ps_banner/1/1/14/99/71/66/997166f0beeafed63ca2a2c262abf67ea3f6f71e.psbannerpsbanner.tpl.php
+++ var/cache/prod/smarty/cache/ps_banner/1/1/14/99/71/66/997166f0beeafed63ca2a2c262abf67ea3f6f71e.psbannerpsbanner.tpl.php
@@ -1,11 +1,11 @@
 <?php
-/* Smarty version 3.1.32, created on 2018-10-06 23:21:52
+/* Smarty version 3.1.32, created on 2018-10-10 19:59:59
   from 'module:psbannerpsbanner.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.32',
-  'unifunc' => 'content_5bb92770150ec1_98581636',
+  'unifunc' => 'content_5bbe3e1f510374_51955117',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
@@ -18,9 +18,9 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   ),
   'cache_lifetime' => 31536000,
 ),true)) {
-function content_5bb92770150ec1_98581636 (Smarty_Internal_Template $_smarty_tpl) {
-?><a class="banner" href="http://127.0.0.1/prestashop/" title="">
-      <img src="http://127.0.0.1/prestashop/modules/ps_banner/img/sale70.png" alt="" title="" class="img-fluid">
+function content_5bbe3e1f510374_51955117 (Smarty_Internal_Template $_smarty_tpl) {
+?><a class="banner" href="https://www2.hm.com/content/hmonline/pl_pl/hmclub.guest.html" title="">
+      <img src="http://127.0.0.1/prestashop/modules/ps_banner/img/6b5b0a67400a7b7e220b9ca78beabfe2.png" alt="" title="" class="img-fluid">
   </a>
 <?php }
 }
