1296407cc284a7cf659537ee409c850f70ae0f82
ssnow <ss.nowacki@gmail.com>
2018-10-17 00:14:49 +0200

    Skonfigurowalem merkurego i naprawilem wysylanie maili

--- mailtest.php
+++ mailtest.php
@@ -1,7 +1,9 @@
 <?php
 $headers =  'MIME-Version: 1.0' . "\r\n"; 
-$headers .= 'From: Your name <prestashop.hm@gmail.com>' . "\r\n";
+$headers .= 'From: Your name <prestashop@hm.pl>' . "\r\n";
 $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
-
-mail($to, $subject, $body, $headers);
+$to = 'prestashop.hm@gmail.com';
+$body = 'elo';
+$subject = 'test';
+mail('prestashop.hm@gmail.com', 'test', 'elo', $headers);
 ?>
